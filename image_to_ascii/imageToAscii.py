e5a1451761dbe69dc8ff53ef4a40996620866d6d
Not Aypro <67575909+aypro-droid@users.noreply.github.com>
2021-03-04 00:00:33 +0530

    Fixed minor typo

--- image_to_ascii/imageToAscii.py
+++ image_to_ascii/imageToAscii.py
@@ -8,14 +8,14 @@ ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 a = ''
 
 class ImageToAscii:
-    def __init__(self,imagePath: str=None,witdh:int=100,outputFile:str=None):
+    def __init__(self,imagePath: str=None,width:int=100,outputFile:str=None):
         '''
         path - The path/name of the image ex: <image_name.png>\n
         width - The width you want the Ascii art to have\n
         outputfile - If you want to store the Ascii art in a txt file then set it to <file_name.txt> else keep it None  
         '''
         self.path = imagePath
-        self.witdh = witdh
+        self.width = width
         try:
             self.image = PIL.Image.open(self.path)
             width, height = self.image.size
@@ -23,13 +23,13 @@ class ImageToAscii:
         except:
             if imagePath is None:
                 print("Invalid path name")
-            elif self.witdh is None:
+            elif self.width is None:
                 print("Invalid Width provided")        
         self.new_image_data = self.pixelsToAscii(self.converToGrayscale(self.resizeImage(self.image)))
 
         self.pixel_count = len(self.new_image_data)
 
-        self.ascii_image = "\n".join([self.new_image_data[index:(index+self.witdh)] for index in range(0, self.pixel_count, self.witdh)])
+        self.ascii_image = "\n".join([self.new_image_data[index:(index+self.width)] for index in range(0, self.pixel_count, self.width)])
         if outputFile is not None:
             with open(outputFile, "w") as f:
                 f.write(self.ascii_image)
@@ -38,8 +38,8 @@ class ImageToAscii:
     def resizeImage(self,image):
         width, height = image.size
         ratio = height/width
-        new_height = int(self.witdh * ratio)
-        resized_image = image.resize((self.witdh, new_height))
+        new_height = int(self.width * ratio)
+        resized_image = image.resize((self.width, new_height))
         
         return(resized_image)
 
